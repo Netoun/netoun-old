3fa656c2a99095266a3769893119aba2738b4b11
netoun <root@DESKTOP-3IG63BA.localdomain>
2021-01-30 02:04:44 +0100

    add zola.sh and gitignore

---
+++ zola.sh
@@ -0,0 +1,4 @@
+wget -q -O - \ "https://github.com/getzola/zola/releases/download/v0.13.0/zola-v0.13.0-x86_64-unknown-linux-gnu.tar.gz" \
+tar xzf - -C /usr/local/bin
+chmod +x ./zola
+zola build --output-dir public
